fd074656da31df55894075593708503c7e24de62
Kai L <kailin8591@gmail.com>
2022-04-20 21:49:54 -0400

    loop completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const collectionOfNames = []
+    for (let i = 0; i < names.length; i++) {
+        collectionOfNames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return collectionOfNames
+}
+
+function countDown (number){
+    while (number >= 0){
+        console.log (number--)
+    }
+   
+}
